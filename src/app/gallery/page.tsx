f6303daa61485927344ab636c9ab16abc7c7809d
kevin <kevin.andres@epitech.eu>
2023-12-21 01:23:33 +0100

    task: add enlarge image in modal

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,12 +3,15 @@
 import useMedias from '@/hooks/useMedias'
 import { MediaType } from '@/models/media/mediaTypes'
 import Gallery from 'react-photo-gallery'
-import { useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import Carousel, { Modal, ModalGateway } from 'react-images'
+import './page.css'
 
 interface Photo {
   src: string
   width: number
   height: number
+  sizes?: string[]
 }
 
 export default function GalleryPage() {
@@ -18,6 +21,20 @@ export default function GalleryPage() {
       type: MediaType.IMAGE,
       pageMaxSize: 25
     })
+  const [currentImage, setCurrentImage] = useState(0)
+  const [viewerIsOpen, setViewerIsOpen] = useState(false)
+
+  const openLightbox = useCallback((event: any, attr: any) => {
+    const { index } = attr
+    setCurrentImage(index)
+    setViewerIsOpen(true)
+  }, [])
+
+  const closeLightbox = () => {
+    setCurrentImage(0)
+    setViewerIsOpen(false)
+  }
+
   const photos: Photo[] =
     medias?.map((media) => {
       const ratio = media.width && media.height ? media.width / media.height : 1
@@ -48,8 +65,25 @@ export default function GalleryPage() {
   return (
     <div>
       {photos.length > 0 && (
-        <Gallery photos={photos} direction={useColumns ? 'column' : undefined} />
+        <Gallery
+          photos={photos}
+          direction={useColumns ? 'column' : undefined}
+          onClick={openLightbox}
+        />
       )}
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map((x) => ({
+                ...x,
+                source: x.src
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
     </div>
   )
 }
